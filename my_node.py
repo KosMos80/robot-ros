bb0d37865a7f91762f2f05302aec5b219e84a7de
kostiantyn Mosin <kostyamosin@ukr.net>
2025-02-05 16:34:47 +0100

    1.01

---
+++ my_node.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(1)  # 1 second message
+
+    while not rospy.is_shutdown():
+        msg = "Hello, world! Time: %s" % rospy.get_time()
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
