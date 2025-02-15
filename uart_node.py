85e599ad7d722d01a37b83399fc6e3c4e5d4e21e
KosMos80 <107849287+KosMos80@users.noreply.github.com>
2025-02-15 23:11:01 +0100

    Update uart_node.py

--- uart_node.py
+++ uart_node.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String, Int8
@@ -10,25 +11,24 @@ BAUDRATE = 9600
 
 
 
-if __name__ == '__main__':
-    #initialize the gpio module
-    try:
-        print("Setup pereferal uart... ")
-        # Open port
-        ser = serial.Serial(UART_PORT, BAUDRATE, timeout=1)
-        print("Open port: ", UART_PORT)
+print("Setup pereferal uart... ")
+# Open port
+ser = serial.Serial(UART_PORT, BAUDRATE, timeout=1)
+print("Open port: ", UART_PORT)
 
+
+if __name__ == '__main__':
+    try:
         rospy.init_node('reader_uart')  # init node
         pub = rospy.Publisher('/uart_data', String, queue_size=10)  # send to topic /cmd_vel
-
         # Read data (if data present)
-        while(1):
+        while True:
             if ser.in_waiting:
                 received_data = ser.readline().decode(errors='ignore').strip()
+                print("Recieve: ", received_data)
                 pub.publish(received_data)
 
-
-
     except rospy.ROSInterruptException:
-        print("exit from node...")
+        ser.close()
+        pass
 
