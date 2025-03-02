6ed35bfb9f8c78cc0c0bb0471254772c378c3a65
KosMos80 <107849287+KosMos80@users.noreply.github.com>
2025-03-02 13:15:40 +0100

    Update uart_node.py
    прием и нормализация данніх углового ускорения и формирования топика азимута

--- uart_node.py
+++ uart_node.py
@@ -1,13 +1,12 @@
-
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String, Int8
+from std_msgs.msg import String, Int16, Float64
 import serial
 import time
 
 # config UART3
 UART_PORT = "/dev/ttyS3"  # or "COM3" for Windows
-BAUDRATE = 9600
+BAUDRATE = 57600
 
 
 
@@ -16,17 +15,40 @@ print("Setup pereferal uart... ")
 ser = serial.Serial(UART_PORT, BAUDRATE, timeout=1)
 print("Open port: ", UART_PORT)
 
+azimut = 0
+sumz = 0
+sigma = 0
+
+cx = 0
 
 if __name__ == '__main__':
     try:
         rospy.init_node('reader_uart')  # init node
-        pub = rospy.Publisher('/uart_data', String, queue_size=10)  # send to topic /cmd_vel
+        pub = rospy.Publisher('/axis', Float64, queue_size=10)  # send to topic /cmd_vel
         # Read data (if data present)
         while True:
-            if ser.in_waiting:
-                received_data = ser.readline().decode(errors='ignore').strip()
-                print("Recieve: ", received_data)
-                pub.publish(received_data)
+            received_data = ser.readline()
+            ln = len(received_data) - 2
+            fst = float(received_data[0:ln]) # - 15.2009
+            dps = fst - 10
+            if(abs(dps) < 60):
+                dps = 0
+
+#            sumz = sumz + fst
+#            sigma = sigma + fst * fst
+#            cx = cx + 1
+#            if(cx == 1000):
+#                delta = sumz / cx
+#                sig = sigma / cx
+#                tresh = (sig - delta * delta) ** 0.5
+#                print(" delta:", delta, "  sigma:", tresh)
+#                sumz = 0
+#                sigma = 0
+#                cx = 0
+            #print("Recieve: ", fst)
+
+            azimut = azimut + dps
+            pub.publish(azimut * 0.00875 / 64)
 
     except rospy.ROSInterruptException:
         ser.close()
