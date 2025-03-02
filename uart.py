e9369ca7c6c546c3aa53be686bce5572cbebf1fe
KosMos80 <107849287+KosMos80@users.noreply.github.com>
2025-03-02 13:17:13 +0100

    Update uart.py

--- uart.py
+++ uart.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 import serial
 import time
 
 # config UART3
 UART_PORT = "/dev/ttyS3"  # or "COM3" for Windows
-BAUDRATE = 9600
+BAUDRATE = 57600
 
 # Open port
 ser = serial.Serial(UART_PORT, BAUDRATE, timeout=1)
@@ -14,16 +15,27 @@ calc = int(0)
 try:
     while True:
         # Send data
-        ser.write(b"Hello UART!\n")
-        print("Send data: Hello UART! Num: ", calc)
-        calc = calc + 1
+        # ser.write(b"Hello UART!\n")
+        # print("Send data: Hello UART! Num: ", calc)
+        # calc = calc + 1
 
         # Read data (if data present)
-        if ser.in_waiting:
-            received_data = ser.readline().decode(errors='ignore').strip()
-            print("Reciev: ", received_data)
+#        if ser.in_waiting:
+            #received_data = ser.read(ser.in_waiting)
+            #received_data = ser.readline().decode(errors='ignore').strip()
+            #received_data = ser.read_until(b'\n')
+        received_data = ser.readline()
+#        print("Reciev: ", received_data)
+        long = len(received_data) - 2
+        st = received_data[0:long]
+#        fst = float(st)
+#        fst = (fst * 9 / 64) / 1024
+#        print(st, " ", fst)
+        print(st)
 
-        time.sleep(1)  # delay 1 s
+
+
+#        time.sleep(1)  # delay 1 s
 
 except KeyboardInterrupt:
     print("\nExit...")
